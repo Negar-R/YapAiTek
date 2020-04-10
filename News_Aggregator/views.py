f4b5d2120c57109f2b5b73581f125eaaf40e409e
Negar-R <negar.r.78.ioi@gmail.com>
2020-04-10 16:18:25 +0430

    write a view for getting news from reddit

--- News_Aggregator/views.py
+++ News_Aggregator/views.py
@@ -1,1 +1,37 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+
+import http.client
+import json
+
+# Create your views here.
+
+def get_reddit_news(request , query):
+
+    if request.method == 'GET':
+        connection = http.client.HTTPSConnection("www.reddit.com")
+
+        if query == 'all_news':
+            connection.request("GET", "/r/news.json")
+        else: 
+            connection.request("GET", "/search.json?q={}".format(query))
+
+        response = connection.getresponse()
+        content = response.read()
+
+        data_json = json.loads(content.decode('utf-8'))
+
+        response_list = []
+
+        for news in data_json['data']['children']:
+            response_list.append(
+                {
+                    "headline" : news['data']['title'],
+                    "link" : news['data']['url'],
+                    "author" : news['data']['author'],
+                    "source" : "reddit"
+                }
+            )
+
+        myJson = {'Response' : response_list}   
+        return JsonResponse(myJson) 
