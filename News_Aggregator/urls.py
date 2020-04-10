d60d807d6deea5aebb20c0cff9b37e9c10f22364
Negar-R <negar.r.78.ioi@gmail.com>
2020-04-10 16:25:46 +0430

    create a seprate file for reddit news

--- News_Aggregator/urls.py
+++ News_Aggregator/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import get_reddit_news
+from .redditViews import get_reddit_news
 
 urlpatterns = [
     path('get_reddit_news/<str:query>' , get_reddit_news),
