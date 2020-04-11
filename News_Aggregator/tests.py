35051cb47fbc588300487a6bb8255e0ca3b3bbaa
Negar-R <negar.r.78.ioi@gmail.com>
2020-04-11 12:11:56 +0430

    write a simple unit test

--- News_Aggregator/tests.py
+++ News_Aggregator/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
+import http.client
+
 # Create your tests here.
+
+class TestExample(TestCase):
+    def setUp(self):
+        self.connection = http.client.HTTPSConnection("www.reddit.com")
+
+    def testView(self):
+        response = self.connection.request("GET", "/r/news")
+        response = self.connection.getresponse()
+        self.assertEqual(response.status, 200)
