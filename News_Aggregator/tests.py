a429f3c61bd254f89a14926e070d89a73ad68e03
Negar-R <negar.r.78.ioi@gmail.com>
2020-04-11 17:48:07 +0430

    provide a test for search request

--- News_Aggregator/tests.py
+++ News_Aggregator/tests.py
@@ -5,8 +5,19 @@ import http.client
 class TestExample(TestCase):
     def setUp(self):
         self.connection = http.client.HTTPSConnection("www.reddit.com")
+        self.words = ['bitcoin' , 'health' , 'exercise' , 'Trump']
 
-    def testView(self):
+    def testGetAllNews(self):
         response = self.connection.request("GET", "/r/news")
         response = self.connection.getresponse()
         self.assertEqual(response.status, 200)
+        self.connection.close()
+
+    def testSearchRequest(self):
+        for query in self.words:
+            response = self.connection.request("GET", "/search?q={}".format(query))
+            response = self.connection.getresponse()
+            self.assertEqual(response.status, 200)
+            print(query)
+            self.connection.close()
+
